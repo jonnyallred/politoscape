4438d7dc8d4dc42524c4772eb42f41e0be3ae06d
Jonathan Allred <jonnyallred@gmail.com>
2011-12-04 01:22:45 -0500

    Little things

--- rss/admin.py
+++ rss/admin.py
@@ -3,7 +3,7 @@ Created on Nov 27, 2011
 
 @author: Jonathan
 '''
-from SpectrumAggregator.rss.models import Entry, Feed
+from politoscape.rss.models import Entry, Feed
 from django.contrib import admin
 
 admin.site.register(Entry)
