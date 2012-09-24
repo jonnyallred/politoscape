92b9d4197e0fa96c67f777ddd49c2abf6323e8f9
Jonathan Allred <jonnyallred@gmail.com>
2012-09-24 00:37:15 -0700

    fixing urls.py

--- spectrum/admin.py
+++ spectrum/admin.py
@@ -3,9 +3,9 @@ Created on Nov 27, 2011
 
 @author: Jonathan
 '''
-from models import Link, Topic, Source
+from models import Story, Topic, Source
 from django.contrib import admin
 
-admin.site.register(Link)
+admin.site.register(Story)
 admin.site.register(Topic)
 admin.site.register(Source)
