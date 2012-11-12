c4eba696a710d745effaedcf721d168f8c6587d3
jonathan <jonnyallred@gmail.com>
2012-11-12 04:50:28 -0700

    The root directory of the website now redirects to /spectrum/

--- politoscape/urls.py
+++ politoscape/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^spectrum/', include('spectrum.urls')),
     (r'^(?P<page_name>about|act|contact|stats|team)/$', 'spectrum.views.static_page'),
+    (r'^$',  lambda x: HttpResponseRedirect('/spectrum/')),
     # Example:
     # (r'^politoscape/', include('politoscape.foo.urls')),
 
